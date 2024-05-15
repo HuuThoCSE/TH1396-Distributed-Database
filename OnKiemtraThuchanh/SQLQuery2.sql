5c22d4de03bab36691dcf3e1e13f5ce4a6cc9ddd
HuuThoCSE <huutho.cse@gmail.com>
2024-05-15 12:05:54 +0700

    Update

---
+++ OnKiemtraThuchanh/SQLQuery2.sql
@@ -0,0 +1,13 @@
+﻿-- Tạo file backup
+BACKUP DATABASE QLHS
+TO DISK = 'D:\HK2_23_24\Backup_file_mau_ver1.bak'
+WITH FORMAT, MEDIANAME = 'SQLServerBackup', name = 'Full Backup of QLHS';
+GO
+
+-- Phục hồi CSDL 
+RESTORE DATABASE QLHS
+FROM DISK = 'D:\HK2_23_24\Backup_file_mau_ver1.bak'
+WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 5;
+GO
+
+
